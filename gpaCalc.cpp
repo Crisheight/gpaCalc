16c968af6cae8b6e706d22509b0d1b1f988fd511
Cris <crisheight@gmail.com>
2024-07-14 15:20:28 -0500

    adjust logic using ranged for loop and pair reference

--- gpaCalc.cpp
+++ gpaCalc.cpp
@@ -5,8 +5,8 @@
 #include "gpaCalc.h"
 
 int gpaCalc::calculate(const std::map<int, int> &intMap){
-    for (int i = 0; i < intMap.size(); i++) {
-        std::cout << intMap.at(i) << "\n";
+    for (const auto& pair : intMap) {
+        std::cout << pair.second << "\n";
     }
     return 0;
 }
