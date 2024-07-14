46d91acde80728431b05ef01fcab4ceeadb28409
Cris <crisheight@gmail.com>
2024-07-14 15:21:13 -0500

    start working on menuDriven function

---
+++ menuDriver.cpp
@@ -0,0 +1,9 @@
+//
+// Created by crisr on 7/14/2024.
+//
+
+#include "menuDriver.h"
+
+int menuDriver::executeOption(const int userChoice) {
+
+};
---
+++ menuDriver.h
@@ -0,0 +1,15 @@
+//
+// Created by crisr on 7/14/2024.
+//
+
+#ifndef GPACALC_MENUDRIVER_H
+#define GPACALC_MENUDRIVER_H
+
+
+class menuDriver {
+public:
+    executeOption(int)
+};
+
+
+#endif //GPACALC_MENUDRIVER_H
