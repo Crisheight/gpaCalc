3f0c54f0d28fc036261500ae5ed413951f445b55
Cris <crisheight@gmail.com>
2024-07-26 20:54:37 -0500

    moved cumulativeGPA logic into function

--- main.cpp
+++ main.cpp
@@ -6,23 +6,9 @@
 
 int main() {
     int menuChoice;
-    int terms;
-    int creditHours;
-    std::map<int, int> creditHoursPerTerm;
 
     menuDriver menu;
     menuChoice = menu.setIntention();
-
-
-
-    std::cout << "GPA Calculator, please enter the number of terms:\n";
-    std::cin >> terms;
-
-    for (int i = 0; i < terms; i++) {
-        std::cout << "Enter the number of credit hours for term " << i + 1 << "\n";
-        std::cin >> creditHours;
-        creditHoursPerTerm[i] += creditHours;
-    }
 
     gpaCalc findMyGPA;
     findMyGPA.calculate(menuChoice);
