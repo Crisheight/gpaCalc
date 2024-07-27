b32a3275e864e5ce22bf0ead64c440e7181717c0
Cris <crisheight@gmail.com>
2024-07-26 20:54:13 -0500

    Added final choice (currentGPA), moved cumulativeGPA logic into function

--- gpaCalc.cpp
+++ gpaCalc.cpp
@@ -6,9 +6,9 @@
 
 float gpaCalc::calculate(const int choice){
     if(choice == 1) {gpaCalc::termGPA();}
-    if(choice == 2) {gpaCalc::cumulativeGPA();}
-    if(choice == 3) {gpaCalc::idealGPA();}
-
+    else if(choice == 2) {gpaCalc::cumulativeGPA();}
+    else if(choice == 3) {gpaCalc::idealGPA();}
+    else {gpaCalc::currentGPA();}
 }
 
 float gpaCalc::termGPA() {
@@ -16,9 +16,26 @@ float gpaCalc::termGPA() {
 }
 
 float gpaCalc::cumulativeGPA() {
+    int terms;
+    int creditHours;
+    std::map<int, int> creditHoursPerTerm;
+
+    std::cout << "Cumulative GPA Calculator, please enter the number of terms:\n";
+    std::cin >> terms;
+
+    for (int i = 0; i < terms; i++) {
+        std::cout << "Enter the number of credit hours for term " << i + 1 << "\n";
+        std::cin >> creditHours;
+        creditHoursPerTerm[i] += creditHours;
+    }
+
     return 0;
 }
 
 float gpaCalc::idealGPA() {
+    return 0;
+}
+
+float gpaCalc::currentGPA() {
     return 0;
 }
