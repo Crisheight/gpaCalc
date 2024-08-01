757acb0b5dd14b0682bc7a414a6579800045216c
Cris <crisheight@gmail.com>
2024-08-01 16:43:32 -0500

    Begin working on handling exit/exit function for the menu to loop.

--- menuDriver.cpp
+++ menuDriver.cpp
@@ -12,17 +12,18 @@ int menuDriver::setIntention() {
     std::cout << "Select an option: \n";
     std::cout << "1. Calculate Semester GPA \n";
     std::cout << "2. Calculate Cumulative GPA \n";
-    std::cout << "3. Calculate Ideal GPA \n";
+    std::cout << "3. Calculate Target GPA \n";
     std::cout << "4. Display Current GPA \n";
+    std::cout << "5. Exit \n";
 
     std::cin >> userChoice;
 
-    while (std::cin.fail() || userChoice < 1 || userChoice > 4) {
+    while (std::cin.fail() || userChoice < 1 || userChoice > 5) {
         std::cin.clear(); // Clear the error state
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
-        std::cout << "Please enter a number between 1 and 4 \n";
+        std::cout << "Please enter a number between 1 and 5 \n";
         std::cin >> userChoice;
     }
 
     return userChoice;
-};
+}
